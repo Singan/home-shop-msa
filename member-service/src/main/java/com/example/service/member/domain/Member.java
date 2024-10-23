2322b79e1033c912c24fc55d6a0c5672d80925a9
sunghee119z@naver.com <sunghee119z@naver.com>
2024-10-23 17:16:55 +0900

    Security 의존성 추가 및 Member 도메인,엔티티 생성
    Auth 분리는 추후 고려할 예정

--- member-service/src/main/java/com/example/service/member/domain/Member.java
+++ member-service/src/main/java/com/example/service/member/domain/Member.java
@@ -1,12 +1,19 @@
 package com.example.service.member.domain;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class Member {
     @NotNull
     private Long id;
+
+    @NotNull
+    private String loginId;
+
+    @NotNull
+    private String password;
     @NotNull
     private String name;
     @NotNull
@@ -15,5 +22,15 @@ public class Member {
     private String phone;
     @NotNull
     private String email;
+    @Builder
 
+    public Member(Long id, String loginId, String password, String name, String address, String phone, String email) {
+        this.id = id;
+        this.loginId = loginId;
+        this.password = password;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
 }
--- member-service/src/main/java/com/example/service/member/infrastructure/entity/MemberEntity.java
+++ member-service/src/main/java/com/example/service/member/infrastructure/entity/MemberEntity.java
@@ -1,4 +1,65 @@
 package com.example.service.member.infrastructure.entity;
 
+import com.example.service.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+
+    private String loginId;
+    private String password;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    @Builder
+    public MemberEntity(Long id, String loginId, String password, String name, String email, String phone, String address) {
+        this.id = id;
+        this.loginId = loginId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public Member toDomain() {
+        return Member
+                .builder()
+                .id(id)
+                .loginId(loginId)
+                .password(password)
+                .name(name)
+                .email(email)
+                .phone(phone)
+                .address(address)
+                .build();
+    }
+
+    public static MemberEntity fromDomain(Member member) {
+        return MemberEntity
+                .builder()
+                .id(member.getId())
+                .password(member.getPassword())
+                .loginId(member.getLoginId())
+                .name(member.getName())
+                .email(member.getEmail())
+                .phone(member.getPhone())
+                .address(member.getAddress())
+                .build();
+
+    }
 }
