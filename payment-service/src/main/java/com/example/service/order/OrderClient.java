c6d5ef74c778d5256c448c4465bf1f32709849b3
sunghee119z@naver.com <sunghee119z@naver.com>
2024-11-07 04:23:13 +0900

    Feign 클라이언트 Order 통신 추가

---
+++ payment-service/src/main/java/com/example/service/order/OrderClient.java
@@ -0,0 +1,15 @@
+package com.example.service.order;
+
+import com.example.service.order.dto.OrderInfoDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Repository
+@FeignClient(name = "order-service", url = "${domain.url}${service.url.order-service}")
+public interface OrderClient {
+
+    @GetMapping("/orders/{orderId}")
+    OrderInfoDto getOrderInfo(@PathVariable("orderId") Long orderId);
+}
---
+++ payment-service/src/main/java/com/example/service/order/dto/OrderInfoDto.java
@@ -0,0 +1,18 @@
+package com.example.service.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class OrderInfoDto {
+
+    private Long productId;
+    private Long userId;
+    private Integer stock;
+    private Long orderId;
+    private LocalDateTime orderTime;
+    private String orderStatus;
+}
