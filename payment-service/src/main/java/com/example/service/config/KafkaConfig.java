007084244d53e7ab75212ac9d2f0bbfa5e905df4
sunghee119z@naver.com <sunghee119z@naver.com>
2024-11-09 02:59:02 +0900

    Payment Kafka 컨피그 수정

--- payment-service/src/main/java/com/example/service/config/KafkaConfig.java
+++ payment-service/src/main/java/com/example/service/config/KafkaConfig.java
@@ -1,15 +1,18 @@
 package com.example.service.config;
 
 import com.example.service.payment.infrastructure.product.dto.StockDecreaseDto;
+import jakarta.validation.Valid;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -18,6 +21,8 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String servers;
 
     @Bean
     public KafkaTemplate<String, StockDecreaseDto> kafkaTemplate1(ProducerFactory<String, StockDecreaseDto> producerFactory1) {
@@ -27,9 +32,11 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, StockDecreaseDto> producerFactory1() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example.service.payment.infrastructure.product.dto");
+        configProps.put(JsonDeserializer.TYPE_MAPPINGS, "stockDecreaseDto:com.example.service.payment.infrastructure.product.dto.StockDecreaseDto");
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
@@ -41,7 +48,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, Long> producerFactory2() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
