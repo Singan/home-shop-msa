171f2f96353021a1efa3777c23bf0c157eed36de
sunghee119z@naver.com <sunghee119z@naver.com>
2024-11-11 00:59:34 +0900

    Client -> Repository 변경

--- order-service/src/main/java/com/example/service/order/application/usecase/OrderRequestUseCase.java
+++ order-service/src/main/java/com/example/service/order/application/usecase/OrderRequestUseCase.java
@@ -12,20 +12,24 @@ import com.example.service.order.domain.enums.OrderStatus;
 import com.example.service.order.domain.repository.OrderRepository;
 import com.example.service.order.infrastructure.product.ProductClient;
 import com.example.service.order.infrastructure.product.dto.ProductDetailDto;
+import com.example.service.order.infrastructure.repository.MemberRepository;
+import com.example.service.order.infrastructure.repository.ProductRepository;
 import com.example.service.order.infrastructure.repository.StockRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderRequestUseCase {
 
     private final OrderValidator orderValidator;
     private final OrderRepository orderRepository;
-    private final ProductClient productClient;
+    private final ProductRepository productRepository;
     private final StockRepository stockRepository;
-    private final MemberClient memberClient;
+    private final MemberRepository memberRepository;
 
     public OrderPlaceResponseDto placeOrder(OrderRequestDto orderRequestDto) {
         // 상품 정보 조회
@@ -37,22 +41,27 @@ public class OrderRequestUseCase {
         // 주문 유효성 검증
         validateOrder(product, orderRequestDto);
 
-        // 주문 저장 및 응답 생성
+        // 캐싱된 재고 감소
+        if (stockRepository.decreaseStock(orderRequestDto.productId(), orderRequestDto.buyStock()) < 0) {
+            throw new OrderStockOutOfException();
+        }
         return saveOrder(orderRequestDto, product, member);
     }
 
     private ProductDetailDto getProductDetails(Long productId) {
         try {
-            return productClient.getProductDetail(productId);
+            return productRepository.getProductDetail(productId);
         } catch (Exception e) {
+            log.info("Error Message : {} , productId = {} , exception = {}", e.getMessage(), productId,e.getClass().toString());
             throw new OrderInvalidParameter();  // 상품 조회 실패시 예외
         }
     }
 
     private MemberInfoDto getMemberProfile() {
         try {
-            return memberClient.getMemberProfile();
+            return memberRepository.getMemberProfile();
         } catch (Exception e) {
+            log.info("Error Message : {} ,  exception = {}", e.getMessage(),e.getClass().toString());
             throw new OrderUnAuthorizedException();  // 사용자 정보 조회 실패시 예외
         }
     }
@@ -64,7 +73,6 @@ public class OrderRequestUseCase {
     }
 
 
-    @Transactional
     protected OrderPlaceResponseDto saveOrder(OrderRequestDto orderRequestDto, ProductDetailDto product, MemberInfoDto member) {
         Order order = Order.builder()
                 .memberId(orderRequestDto.userId())
@@ -73,12 +81,7 @@ public class OrderRequestUseCase {
                 .quantity(orderRequestDto.buyStock())
                 .status(OrderStatus.PENDING)
                 .build();
-
         order = orderRepository.saveOrder(order);
-
-        if (stockRepository.decreaseStock(orderRequestDto.productId(), orderRequestDto.buyStock()) < 0) {
-            throw new OrderStockOutOfException();
-        }
         return OrderServiceFactory.createOrderPlaceResponseDto(order, product, orderRequestDto.userId(), member);
     }
 }
