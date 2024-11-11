bfa64be0ed3541553f1516c8b24dc89b4a12557a
sunghee119z@naver.com <sunghee119z@naver.com>
2024-11-11 14:30:06 +0900

    Thread Pool 적정 설정 변경

---
+++ order-service/src/main/java/com/example/service/config/ThreadPoolConfig.java
@@ -0,0 +1,25 @@
+package com.example.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+public class ThreadPoolConfig implements AsyncConfigurer {
+    @Bean
+    public TaskExecutor taskExecutor(){
+        ThreadPoolTaskExecutor threadPoolExecutor = new ThreadPoolTaskExecutor();
+        threadPoolExecutor.setCorePoolSize(200);
+        threadPoolExecutor.setKeepAliveSeconds(60);
+        threadPoolExecutor.setThreadNamePrefix("my-executor-");
+        threadPoolExecutor.initialize();
+        return threadPoolExecutor;
+    }
+
+}
